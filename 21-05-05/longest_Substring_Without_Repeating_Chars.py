15f98763411302fe6a98356e83ef4a25099f89a9
MAYANK KHATRI <modimayank7@gmail.com>
2023-05-21 19:24:23 +0530

    python programs done at 21 may

---
+++ 21-05-05/longest_Substring_Without_Repeating_Chars.py
@@ -0,0 +1,43 @@
+def longestSubstringWithoutRepeatingChars(s):
+    n = len(s)
+    max_length = 0
+    left = 0
+    right = 0
+    chars_set = set()
+
+    while right < n:
+        if s[right] not in chars_set:
+            chars_set.add(s[right])
+            max_length = max(max_length, right - left + 1)
+            right += 1
+        else:
+            chars_set.remove(s[left])
+            left += 1
+
+    return max_length
+print(longestSubstringWithoutRepeatingChars('bbb'))
+
+'''
+def longestSubstringWithoutRepeatingChars(s):
+    n = len(s)
+    max_length = 0
+    start = 0
+    end = 0
+    chars_set = set()
+
+    while end < n:
+        if s[end] not in chars_set:
+            chars_set.add(s[end])
+            if end - start + 1 > max_length:
+                max_length = end - start + 1
+                substring = s[start:end+1]
+            end += 1
+        else:
+            chars_set.remove(s[start])
+            start += 1
+        print(substring)
+        print(chars_set)
+    return substring
+
+s = "crgcer"
+print(longestSubstringWithoutRepeatingChars(s))  # Output: "abc"
